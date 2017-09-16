718f992fb301e233d1910eccede1984f2d3e2abc
kmok002 <kafungmok002@gmail.com>
2017-09-15 22:48:41 -0400

    Update sample_line.py

--- sample_line.py
+++ sample_line.py
@@ -4,11 +4,7 @@ Created on Thu Aug 31 22:05:55 2017
 
 @author: Kafung
 """
-import random
-from random import randrange, uniform
 import numpy as np 
-from numpy import linalg as la
-
 def sample_line(n):
     
     dir_vec = np.random.randn(3) ;
