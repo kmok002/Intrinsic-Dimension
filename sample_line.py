1626262a732c371dab5fa29deef42560e7aa5a79
kmok002 <kafungmok002@gmail.com>
2017-09-17 13:16:26 -0400

    Update sample_line.py

--- sample_line.py
+++ sample_line.py
@@ -5,12 +5,14 @@ Created on Thu Aug 31 22:05:55 2017
 @author: Kafung
 """
 import numpy as np 
+
+# Outputs n sample points from a random line in R^3 as a matrix of the form nx3. 
 def sample_line(n):
     
-    dir_vec = np.random.randn(3) ;
-    line_point = np.random.randn(3) ;
-    parameter = np.transpose(np.random.randn(n)[np.newaxis]) ;
-    line_points = line_point + parameter*dir_vec ;
+    dir_vec = np.random.randn(3)
+    line_point = np.random.randn(3)  
+    parameter = np.transpose(np.random.randn(n)[np.newaxis]) 
+    line_points = line_point + parameter*dir_vec 
       
     return line_points
     
